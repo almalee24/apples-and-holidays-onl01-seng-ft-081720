ad7aba7b84fcbac458923e5a284f0d8c8676b36e
Alma Flores <flores.almalee@gmail.com>
2020-08-19 23:34:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     
     value.each do |key, value|
-      puts " #{key.to_s.capitalize}: #{value.join(', ')}"
+      puts "  #{key.to_s.capitalize}: #{value.join(', ')}"
     end
   end
 end
